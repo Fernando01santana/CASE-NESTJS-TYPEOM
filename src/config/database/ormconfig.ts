4c1c3bed3a468d7bf93cc7b31cd7562471b1f1ec
Fernando01santana <cool.fernando23@gmail.com>
2022-10-28 00:31:26 -0300

    feat: create enviroments

--- src/config/database/ormconfig.ts
+++ src/config/database/ormconfig.ts
@@ -1,14 +1,16 @@
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 import { DataSource } from 'typeorm';
+import {config} from 'dotenv'
+config()
 
 
  const ormConfig = new DataSource({
     type: "postgres",
-    host: "localhost",
-    port: 5432,
-    username: "postgres",
-    password: "123",
-    database: "EP",
+    host: process.env.HOST,
+    port: parseInt(process.env.PORT),
+    username: process.env.USERNAME,
+    password: String(process.env.PASSWORD),
+    database: process.env.DATABASE,
     entities: [],
     migrations: ["dist/shared/typeorm/migrations/*.js"],
     // cli:{migrationsDir:"dist/shared/typeorm/migrations/*.js"},
